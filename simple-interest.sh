4c51d1a4492b88f51c2322011a30935c3c2f5a68
AKHIL-ALAL <alalakhil29@gmail.com>
2025-11-24 23:32:18 +0530

    Create simple-interest.sh for calculating simple interest
    Added a simple interest calculator script with usage instructions.

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+# Formula: (P * R * T) / 100
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <principal> <rate> <time>"
+  exit 1
+fi
+
+P=$1
+R=$2
+T=$3
+
+SI=$(echo "$P $R $T" | awk '{print ($1*$2*$3)/100}')
+
+echo "The simple interest is: $SI"
 
